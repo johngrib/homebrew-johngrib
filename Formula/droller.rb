9880673ce59c4802f680fff9d413703cdd25e0be
John Grib <johngrib82@gmail.com>
2019-11-30 10:13:43 +0900

    Update droller to v1.1.0

--- Formula/droller.rb
+++ Formula/droller.rb
@@ -1,8 +1,8 @@
 class Droller < Formula
   desc "Droller"
   homepage "https://github.com/johngrib/droller/"
-  url "https://github.com/johngrib/droller/archive/v1.0.2.tar.gz"
-  sha256 "c5a610af0e6c9b144ded052789d666c1ad71270ee0b89b45b0825ac73f3f19c1"
+  url "https://github.com/johngrib/droller/archive/v1.1.0.tar.gz"
+  sha256 "c7092233b2203437dca3e6cbaef34708ae4ffc6c0ce94ebdbc6a7789dd6c722e"
 
   def install
     bin.install "./bin/droller"

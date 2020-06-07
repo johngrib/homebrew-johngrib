81b6971b31583a6872087d3063ca3671e2e9ddff
John Grib <johngrib82@gmail.com>
2020-06-07 13:19:38 +0900

    Update fav to 2.0.1

--- Formula/fav.rb
+++ Formula/fav.rb
@@ -1,12 +1,12 @@
 class Fav < Formula
   desc "Fav"
   homepage "https://github.com/johngrib/fav-dir/"
-  url "https://github.com/johngrib/fav-dir/archive/2.0.0.tar.gz"
+  url "https://github.com/johngrib/fav-dir/archive/2.0.1.tar.gz"
   sha256 "5c10b10a13d359257365c502462d46832145718d762ccade741bae783921cdcc"
 
   depends_on "fzf"
 
   def install
-    bin.install "./bin/fav"
+    bin.install "./bin/fav.sh"
   end
 end
